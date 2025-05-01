9e1e32c8b717508d95a7ecd449fc721095a374d6
aarslantas <a_arslantas69@hotmail.com>
2025-05-01 12:02:54 +0300

    feat: enhance POST /api/v1/tours endpoint to save new tours to file

--- app.js
+++ app.js
@@ -27,13 +27,22 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: req.body,
-    },
-  });
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
