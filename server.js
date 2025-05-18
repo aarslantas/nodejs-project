d86c679cf2aa1d3496e4140eb670133a6e46f0cc
aarslantas <a_arslantas69@hotmail.com>
2025-05-18 12:57:36 +0300

    feat: update environment configuration and enhance database connection setup in server.js

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose'); // Import mongoose for MongoDB object modeling
-const app = require('./app'); // Import the Express app from app.js
 const dotenv = require('dotenv'); // Import dotenv for environment variable management
+const app = require('./app'); // Import the Express app from app.js
 
 dotenv.config({ path: './config.env' }); // Load environment variables from config.env
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  '<db_password>',
   process.env.DATABASE_PASSWORD,
 ); // Replace <PASSWORD> in the database URL with the actual password
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!')) // Log success message on successful connection
-  .catch((err) => console.error('DB connection error:', err)); // Log error message on connection failure
+  .catch((err) => console.error('DB connection error:', err));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
 const PORT = process.env.PORT || 3000; // Set the port to the value from environment variables or default to 3000
 app.listen(PORT, () => {
   console.log('server is running on port 3000');
