36270a0327c1aa5a9d9069d92828d8f3d481b9bc
aarslantas <a_arslantas69@hotmail.com>
2025-05-20 17:30:01 +0300

    feat: add mongoose dependency and remove test Tour model creation from server.js

--- server.js
+++ server.js
@@ -15,19 +15,6 @@ mongoose
   .then(() => console.log('DB connection successful!')) // Log success message on successful connection
   .catch((err) => console.error('DB connection error:', err));
 
-// Create a Tour model based on the schema
-
-const testTour = new Tour({
-  name: 'The Park Camper 4',
-  price: 997,
-  rating: 4.7,
-});
-
-testTour
-  .save()
-  .then(() => console.log('Tour saved successfully!')) // Log success message on successful save
-  .catch((err) => console.error('Error saving tour:', err)); // Log error message on failure
-
 const PORT = process.env.PORT || 3000; // Set the port to the value from environment variables or default to 3000
 app.listen(PORT, () => {
   console.log('server is running on port 3000');
