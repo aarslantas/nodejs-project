87b650c910d23ea35fc4c436a8ab9bd1a6df73af
aarslantas <a_arslantas69@hotmail.com>
2025-06-01 16:13:56 +0300

    feat: add route for tour statistics endpoint

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 router.route('/').get(getAllTours).post(createTour);
 
